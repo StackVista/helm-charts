c8941491f4cd71d147509486987446e11454a425
Remco Beckers <remco.beckers@gmail.com>
2020-04-08 09:06:20 +0200

    STAC-7331 Generator creating pullSecretDockerConfigJson

--- stable/stackstate/installation/generate_values.sh
+++ stable/stackstate/installation/generate_values.sh
@@ -2,10 +2,25 @@
 
 set -e
 
+#colors
+
+nc="\033[0m"
+red="\\033[0;31m"
+green="\\033[0;32m"
 
 function usage() {
-  echo "Generate a values.yaml for deploying StackState to Kubernetes with Helm 3."
-  echo "TODO: more explanation"
+  cat <<EOF
+Generate a values.yaml for deploying StackState to Kubernetes with Helm 3.
+If any of the required arguments are missing they will be asked interactively.
+
+Arguments:
+    -i : Image pull secret .dockerConfigJson property (required)
+    -l : StackState license key (required)
+    -u : StackState base URL; i.e. public (outside of the Kubernetes cluster) URL of StackState (required)
+    -p : Administrator password (required)
+    -v : Name of generated values file (default: values.yaml)
+    -h : Show this help text
+EOF
 }
 
 values_file="values.yaml"
@@ -13,7 +28,7 @@ values_file="values.yaml"
 # Parse arguments
 while getopts "i:l:s:u:p:v:h" opt; do
   case "$opt" in
-  i)  image_pull_secret=$OPTARG ;;
+  i)  image_pull_secret_docker_config_json=$OPTARG ;;
   l)  license_key=$OPTARG ;;
   u)  url=$OPTARG ;;
   p)  admin_password=$OPTARG ;;
@@ -27,16 +42,16 @@ done
 
 function check_args() {
   [ -z "${license_key}" ] && read -r -p "Please provide the license key (-l): " license_key
-  [ -z "${license_key}" ] && echo "License key (-l) is a required argument." && exit 1
+  [ -z "${license_key}" ] && echo -e "${red}License key (-l) is a required argument.${nc}" && exit 1
 
-  [ -z "${image_pull_secret}" ] && read -r -p "Please provide the imagePullSecret (-i): " image_pull_secret
-  [ -z "${image_pull_secret}" ] && echo "image_pull_secret (-i) is a required argument." && exit 1
+  [ -z "${image_pull_secret_docker_config_json}" ] && read -r -p "Please provide the image pull secret json (-i): " image_pull_secret_docker_config_json
+  [ -z "${image_pull_secret_docker_config_json}" ] && echo -e "${red}image pul secret json (-i) is a required argument.${nc}" && exit 1
 
   [ -z "${url}" ] && read -r -p "Please provide the base URLfor StackState (-u): " url
-  [ -z "${url}" ] && echo "The base url (-u) is a required argument." && exit 1
+  [ -z "${url}" ] && echo -e "${red}The base url (-u) is a required argument.${nc}" && exit 1
 
   [ -z "${admin_password}" ] && read -r -p "Please provide the administrator password (-p): " admin_password
-  [ -z "${admin_password}" ] && echo "The administrator password (-p) is a required argument." && exit 1
+  [ -z "${admin_password}" ] && echo -e "${red}The administrator password (-p) is a required argument.${nc}" && exit 1
 
   return 0
 }
@@ -63,7 +78,7 @@ stackstate:
   components:
     all:
     image:
-      pullSecret: "${image_pull_secret}"
+      pullSecretDockerConfigJson: "${image_pull_secret_docker_config_json}"
     server:
       extraEnv:
         secret:
@@ -73,22 +88,30 @@ stackstate:
     apiKey: "$(generate_api_key)"
   license:
     key: "${license_key}"
+hbase:
+  components:
+    all:
+      image:
+        pullSecretDockerConfigJson: "${image_pull_secret_docker_config_json}"
 EOF
 }
 
 function print_helm_command() {
-  cat <<EOF
-Generated '${values_file}'.
-
-Make sure to store this file in a safe place for usage during upgrades of
-StackState. Generating a new file will generate a new API key which
-would require updating all running agents and other clients.
-
-Use the following command to install or upgrade StackState into
-namespace 'stackstate' using helm release 'stackstate':
-
-helm upgrade --install --namespace stackstate --values values.yaml stackstate stackstate/stackstate
-EOF
+  echo -e "Generated ${green}'${values_file}'${nc}."
+  echo -e ""
+  echo -e "Make sure to ${green}store this file in a safe place${nc} for usage during upgrades of"
+  echo -e "StackState. Generating a new file will generate a new API key which"
+  echo -e "would require updating all running agents and other clients."
+  echo -e ""
+  echo -e "Now for first time installation create a namespace for StackState first:"
+  echo -e ""
+  echo -e "${green}kubectl create namespace stackstate${nc}"
+  echo -e ""
+  echo -e "Use the following command to install or upgrade StackState into"
+  echo -e "namespace 'stackstate' using helm release 'stackstate':"
+  echo -e ""
+  echo -e "${green}helm upgrade --install --namespace stackstate --values values.yaml stackstate stackstate/stackstate${nc}"
+  echo -e ""
 }
 
 check_helm
