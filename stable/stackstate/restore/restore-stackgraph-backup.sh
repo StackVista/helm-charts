7719caabec17b1b162a5e6a2370f728a5671690b
Vincent Partington <vpartington@stackstate.com>
2021-02-18 14:51:16 +0100

    STAC-11548 Fixes MinIO for AWS IAM roles
    - Upgrades minio Helm chart to 8.0.10
    - Sets default # of replicas for S3 and Azure gateway to 1.
    - Checks for existence of bucket before creating it
    - Simplifies while loops in restore scripts

--- stable/stackstate/restore/restore-stackgraph-backup.sh
+++ stable/stackstate/restore/restore-stackgraph-backup.sh
@@ -3,15 +3,13 @@ set -Eeuo pipefail
 
 BACKUP_FILE=$1
 
-JOB_YAML_DIR=$(mktemp -d /tmp/sts-restore-XXXXXX)
-# shellcheck disable=SC2064
-trap "rm -rf ${JOB_YAML_DIR}" EXIT
-
 JOB_NAME_TEMPLATE=stackgraph-restore-backup
 JOB_NAME=stackgraph-restore-$(date +%Y%m%dt%H%M%S)
+JOB_YAML_DIR=$(mktemp -d /tmp/sts-restore-XXXXXX)
 JOB_YAML_FILE="${JOB_YAML_DIR}/job-${JOB_NAME}.yaml"
 
 kubectl get configmap stackstate-backup-restore-scripts -o jsonpath="{.data.job-${JOB_NAME_TEMPLATE}\.yaml}"  | sed -e "s/${JOB_NAME_TEMPLATE}/${JOB_NAME}/" -e "s/REPLACE_ME_BACKUP_FILE_REPLACE_ME/${BACKUP_FILE}/" > "${JOB_YAML_FILE}"
 kubectl create -f "${JOB_YAML_FILE}"
-while [[ ! $(kubectl logs job/"${JOB_NAME}") ]]; do echo "Waiting for job to start..."; sleep 2; done; kubectl logs "job/${JOB_NAME}" -f
+while ! kubectl logs job/"${JOB_NAME}" >/dev/null 2>/dev/null ; do echo "Waiting for job to start..."; sleep 2; done; kubectl logs "job/${JOB_NAME}" -f
 kubectl delete job "${JOB_NAME}"
+rm -rf "${JOB_YAML_DIR}"
--- stable/stackstate/scripts/list-elasticsearch-snapshots.sh
+++ stable/stackstate/scripts/list-elasticsearch-snapshots.sh
@@ -3,3 +3,4 @@ set -Eeuo pipefail
 
 echo "=== Listing ElasticSearch snapshots in snapshot repository \"${BACKUP_ELASTICSEARCH_SNAPSHOT_REPOSITORY_NAME}\" in bucket \"${BACKUP_ELASTICSEARCH_BUCKET_NAME}\"..."
 curl --request GET "http://${ELASTICSEARCH_ENDPOINT}/_snapshot/${BACKUP_ELASTICSEARCH_SNAPSHOT_REPOSITORY_NAME}/_all" --fail --silent --show-error | jq -r ".snapshots[] | .snapshot"
+echo "==="
