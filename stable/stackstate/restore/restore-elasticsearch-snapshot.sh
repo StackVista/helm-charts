cdca011ada793b6cd00eec2b89c40417750dbf48
Remco Beckers <remco.beckers@gmail.com>
2021-03-08 15:42:37 +0100

    STAC-12178 Fixes PVC for temp data in stackgraph backup
    - Fixes permissions on PVC for backup job
    - Adds PVC for restore job

--- stable/stackstate/restore/restore-elasticsearch-snapshot.sh
+++ stable/stackstate/restore/restore-elasticsearch-snapshot.sh
@@ -9,7 +9,7 @@ JOB_YAML_DIR=$(mktemp -d /tmp/sts-restore-XXXXXX)
 JOB_YAML_FILE="${JOB_YAML_DIR}/job-${JOB_NAME}.yaml"
 
 if (! (kubectl get configmap stackstate-backup-restore-scripts -o jsonpath="{.data.job-${JOB_NAME_TEMPLATE}\.yaml}"  | sed -e "s/${JOB_NAME_TEMPLATE}/${JOB_NAME}/" -e "s/REPLACE_ME_SNAPSHOT_NAME_REPLACE_ME/${SNAPSHOT_NAME}/" > "${JOB_YAML_FILE}")) || [ ! -s "${JOB_YAML_FILE}" ]; then
-    echo "Did you set backup.elasticsearch.restore.enabled to true?"
+    echo "Did you set backup.enabled and backup.elasticsearch.restore.enabled to true?"
     exit 1
 fi
 
