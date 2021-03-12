af15edb8a77e928db1d6a672fa43181058d912d9
Alejandro Acevedo <aacevedo@stackstate.com>
2021-03-12 10:01:03 +0100

    STAC-11917: Make force delete flag during restart configurable

--- stable/stackstate/restore/restore-stackgraph-backup.sh
+++ stable/stackstate/restore/restore-stackgraph-backup.sh
@@ -3,12 +3,17 @@ set -Eeuo pipefail
 
 BACKUP_FILE=$1
 
+FORCE_DELETE=""
+if [ "${2:-}" = "-force" ]; then
+  FORCE_DELETE=$2
+fi;
+
 JOB_NAME_TEMPLATE=stackgraph-restore-backup
 JOB_NAME=stackgraph-restore-$(date +%Y%m%dt%H%M%S)
 JOB_YAML_DIR=$(mktemp -d /tmp/sts-restore-XXXXXX)
 JOB_YAML_FILE="${JOB_YAML_DIR}/job-${JOB_NAME}.yaml"
 
-if (! (kubectl get configmap stackstate-backup-restore-scripts -o jsonpath="{.data.job-${JOB_NAME_TEMPLATE}\.yaml}"  | sed -e "s/${JOB_NAME_TEMPLATE}/${JOB_NAME}/" -e "s/REPLACE_ME_BACKUP_FILE_REPLACE_ME/${BACKUP_FILE}/" > "${JOB_YAML_FILE}")) || [ ! -s "${JOB_YAML_FILE}" ]; then
+if (! (kubectl get configmap stackstate-backup-restore-scripts -o jsonpath="{.data.job-${JOB_NAME_TEMPLATE}\.yaml}"  | sed -e "s/${JOB_NAME_TEMPLATE}/${JOB_NAME}/" -e "s/REPLACE_ME_BACKUP_FILE_REPLACE_ME/${BACKUP_FILE}/" -e "s/REPLACE_ME_FORCE_DELETE/${FORCE_DELETE}/" > "${JOB_YAML_FILE}")) || [ ! -s "${JOB_YAML_FILE}" ]; then
     echo "Did you set backup.stackGraph.restore.enabled to true?"
     exit 1
 fi
