d132ebb69cbff7e5e00dd25b89740decf7f2d6e3
Jeroen van Erp <jeroen@hierynomus.com>
2021-11-26 09:49:51 +0100

    STAC-0: Fix sts chart release version

--- .gitlab/bump_sts_chart_master_version.sh
+++ .gitlab/bump_sts_chart_master_version.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 repository=$1
 
 latest_version=$(helm search repo --devel  "$repository/stackstate" -o json | jq --arg chart "$repository/stackstate" '.[] | select (.name == $chart) | .version' -r)
-latest_version_counter=$(sed -E -e 's/^(.*\.)([0-9]+)$/\2/' <<< "$latest_version")
+latest_version_counter=$(sed -E -e 's/^(.*\.)([0-9]+)(-.*)?$/\2/' <<< "$latest_version")
 new_version_counter=$((latest_version_counter+1))
 
-sed -i -E -e "s/^(version: [0-9]+\.[0-9]+\.[0-9]+-snapshot\.)[0-9]+/\1${new_version_counter}/" stable/stackstate/Chart.yaml
+sed -i -E -e "s/^(version: [0-9]+\.[0-9]+\.[0-9]+-snapshot\.)[0-9]+(-.*)?/\1${new_version_counter}/" stable/stackstate/Chart.yaml
