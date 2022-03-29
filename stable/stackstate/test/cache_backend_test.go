93f5fbf52e2bc22e904d024401aae2c8ad6a329c
Pedro Jordao <pedrohjordao@gmail.com>
2022-03-29 15:07:00 +0200

    Adds tests

---
+++ stable/stackstate/test/cache_backend_test.go
@@ -0,0 +1,63 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/StackVista/DevOps/helm-charts/helmtestutil"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestSyncWithInMemoryCache(t *testing.T) {
+	output := helmtestutil.RenderHelmTemplate(t, "stackstate", "values/sync_inmemory.yaml")
+
+	resources := helmtestutil.NewKubernetesResources(t, output)
+
+	var stsSyncDeployment appsv1.Deployment
+
+	for _, deployment := range resources.Deployments {
+		if deployment.Name == "stackstate-sync" {
+			stsSyncDeployment = deployment
+		}
+	}
+	assert.NotNil(t, stsSyncDeployment)
+	expected := v1.EnvVar{Name: "CONFIG_FORCE_stackstate_cacheStorage_backend", Value: "inmemory"}
+	assert.Contains(t, stsSyncDeployment.Spec.Template.Spec.Containers[0].Env, expected)
+}
+
+func TestSyncWithMapDbCache(t *testing.T) {
+	output := helmtestutil.RenderHelmTemplate(t, "stackstate", "values/sync_mapdb.yaml")
+
+	resources := helmtestutil.NewKubernetesResources(t, output)
+
+	var stsSyncDeployment appsv1.Deployment
+
+	for _, deployment := range resources.Deployments {
+		if deployment.Name == "stackstate-sync" {
+			stsSyncDeployment = deployment
+		}
+	}
+	assert.NotNil(t, stsSyncDeployment)
+	expected := v1.EnvVar{Name: "CONFIG_FORCE_stackstate_cacheStorage_backend", Value: "mapdb"}
+	assert.Contains(t, stsSyncDeployment.Spec.Template.Spec.Containers[0].Env, expected)
+}
+
+func TestSyncWithRocksDbCache(t *testing.T) {
+	output := helmtestutil.RenderHelmTemplate(t, "stackstate", "values/sync_rocksdb.yaml")
+
+	resources := helmtestutil.NewKubernetesResources(t, output)
+
+	var stsSyncDeployment appsv1.Deployment
+
+	for _, deployment := range resources.Deployments {
+		if deployment.Name == "stackstate-sync" {
+			stsSyncDeployment = deployment
+		}
+	}
+	assert.NotNil(t, stsSyncDeployment)
+	expected := v1.EnvVar{Name: "CONFIG_FORCE_stackstate_cacheStorage_backend", Value: "rocksdb"}
+	assert.Contains(t, stsSyncDeployment.Spec.Template.Spec.Containers[0].Env, expected)
+	expectedBytes := v1.EnvVar{Name: "CONFIG_FORCE_stackstate_rocksdb_cacheSizeBytes", Value: 1166666}
+	assert.Contains(t, stsSyncDeployment.Spec.Template.Spec.Containers[0].Env, expectedBytes)
+}
