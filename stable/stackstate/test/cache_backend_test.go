18dcdc6bb23ce90195cd2b74802e74fd4c702bc4
Anand Singh <asingh@stackstate.com>
2022-06-15 13:27:46 +0000

    STAC-16839 Increase health-sync memory

--- stable/stackstate/test/cache_backend_test.go
+++ stable/stackstate/test/cache_backend_test.go
@@ -111,6 +111,6 @@ func TestHealthSyncWithRocksDbCache(t *testing.T) {
 	assert.NotNil(t, stsSyncDeployment)
 	expected := v1.EnvVar{Name: "CONFIG_FORCE_stackstate_cacheStorage_backend", Value: "rocksdb"}
 	assert.Contains(t, stsSyncDeployment.Spec.Template.Spec.Containers[0].Env, expected)
-	expectedBytes := v1.EnvVar{Name: "CONFIG_FORCE_stackstate_rocksdb_cacheSizeBytes", Value: "225000000" }
+	expectedBytes := v1.EnvVar{Name: "CONFIG_FORCE_stackstate_rocksdb_cacheSizeBytes", Value: "300000000" }
 	assert.Contains(t, stsSyncDeployment.Spec.Template.Spec.Containers[0].Env, expectedBytes)
 }
