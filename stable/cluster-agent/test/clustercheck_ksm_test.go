f5d6d0f700d6e319997263cee68223ee15b7cffd
Alexander Hasselbach <agasselbakh@stackstate.com>
2022-07-25 16:27:22 +0300

    STAC-16347 fix existing test, add new one and update stackstate dependency lock file

--- stable/cluster-agent/test/clustercheck_ksm_test.go
+++ stable/cluster-agent/test/clustercheck_ksm_test.go
@@ -14,8 +14,8 @@ func TestShouldRenderConfigMapWithoutOverrides(t *testing.T) {
 
 	assert.Contains(t, resources.ConfigMaps, "cluster-agent")
 	cm := resources.ConfigMaps["cluster-agent"]
-	assert.Contains(t, cm.Data, "_etc_stackstate-agent_conf.d_kubernetes_state.d_conf.yaml")
-	assert.Contains(t, cm.Data["_etc_stackstate-agent_conf.d_kubernetes_state.d_conf.yaml"], "kube_state_url: http://cluster-agent-kube-state-metrics.")
+	assert.Contains(t, cm.Data, "kubernetes_state_conf")
+	assert.Contains(t, cm.Data["kubernetes_state_conf"], "kube_state_url: http://cluster-agent-kube-state-metrics.")
 }
 
 func TestShouldRenderConfigMapWithKSMWithCustomUrl(t *testing.T) {
@@ -25,8 +25,8 @@ func TestShouldRenderConfigMapWithKSMWithCustomUrl(t *testing.T) {
 
 	assert.Contains(t, resources.ConfigMaps, "cluster-agent")
 	cm := resources.ConfigMaps["cluster-agent"]
-	assert.Contains(t, cm.Data, "_etc_stackstate-agent_conf.d_kubernetes_state.d_conf.yaml")
-	assert.Contains(t, cm.Data["_etc_stackstate-agent_conf.d_kubernetes_state.d_conf.yaml"], "kube_state_url: http://my-custom-ksm-url.")
+	assert.Contains(t, cm.Data, "kubernetes_state_conf")
+	assert.Contains(t, cm.Data["kubernetes_state_conf"], "kube_state_url: http://my-custom-ksm-url.")
 }
 func TestShouldRenderConfigMapWithoutKSMWithCustomUrl(t *testing.T) {
 	output := helmtestutil.RenderHelmTemplate(t, "cluster-agent", "values/minimal.yaml", "values/clustercheck_no_ksm_custom_url.yaml")
@@ -35,8 +35,8 @@ func TestShouldRenderConfigMapWithoutKSMWithCustomUrl(t *testing.T) {
 
 	assert.Contains(t, resources.ConfigMaps, "cluster-agent")
 	cm := resources.ConfigMaps["cluster-agent"]
-	assert.Contains(t, cm.Data, "_etc_stackstate-agent_conf.d_kubernetes_state.d_conf.yaml")
-	assert.Contains(t, cm.Data["_etc_stackstate-agent_conf.d_kubernetes_state.d_conf.yaml"], "kube_state_url: http://my-custom-ksm-url.")
+	assert.Contains(t, cm.Data, "kubernetes_state_conf")
+	assert.Contains(t, cm.Data["kubernetes_state_conf"], "kube_state_url: http://my-custom-ksm-url.")
 	assert.NotContains(t, resources.Deployments, "cluster-agent-kube-state-metrics")
 }
 func TestShouldRenderConfigMapWithKSMWithOverride(t *testing.T) {
