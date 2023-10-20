ec1a370b6832a3395d779c448137653adef3baba
Lukasz Marchewka <lukasz.marchewka@gmail.com>
2023-10-20 11:05:15 +0200

    STAC-19837 Handle missing CI_COMMIT_BRANCH for tags

--- .gitlab/util.sh
+++ .gitlab/util.sh
@@ -19,10 +19,12 @@ function commit_changes() {
 }
 
 function push_changes() {
+  branch=${CI_COMMIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)} # tags don't have CI_COMMIT_BRANCH, so I fetches the current branch from git
+
   if [[ "${PROMOTION_DRY_RUN}" == 'no' ]]; then
     echo "Pushing changes"
-    git pull --rebase origin "${CI_COMMIT_BRANCH}"
-    git push "https://gitlab-ci-token:${gitlab_api_scope_token:?}@gitlab.com/stackvista/devops/helm-charts.git" HEAD:"${CI_COMMIT_BRANCH}"
+    git pull --rebase origin "${branch}"
+    git push "https://gitlab-ci-token:${gitlab_api_scope_token:?}@gitlab.com/stackvista/devops/helm-charts.git" HEAD:"${branch}"
   else
     echo "Not pushing changes, set PROMOTION_DRY_RUN='no' to commit changes"
   fi
