8f54f0e072e982de8629cd4d42213030d8bd0577
Vladimir Iliakov <viliakov@stackstate.com>
2024-03-01 08:56:01 +0100

    STAC-20583: Use new Alpine-based Devops image with sts-toolbox instead of Google CLI

--- .gitlab/aws_auth_setup.sh
+++ .gitlab/aws_auth_setup.sh
@@ -4,8 +4,8 @@
 # https://github.com/aws/amazon-eks-pod-identity-webhook/issues/8
 
 new_token_file="/$CI_PROJECT_DIR/aws_token"
-sudo cp "${AWS_WEB_IDENTITY_TOKEN_FILE}" "${new_token_file}"
-sudo chmod a+r "${new_token_file}"
+cp "${AWS_WEB_IDENTITY_TOKEN_FILE}" "${new_token_file}"
+chmod a+r "${new_token_file}"
 
 # Ignore the settings from the environment such that the config file is used instead
 export AWS_WEB_IDENTITY_TOKEN_FILE="${new_token_file}"
