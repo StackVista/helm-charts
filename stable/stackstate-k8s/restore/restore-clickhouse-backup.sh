87a2ffd63267aaec5e4a5d269c5d0a9cdae05875
Lukasz Marchewka <lukasz.marchewka@gmail.com>
2024-07-22 00:16:22 +0200

    STAC-21413 Improved restore script

--- stable/stackstate-k8s/restore/restore-clickhouse-backup.sh
+++ stable/stackstate-k8s/restore/restore-clickhouse-backup.sh
@@ -8,19 +8,23 @@ fi
 
 BACKUP_NAME=$1
 
+echo "=== Scaling down all OTel collectors"
 collectorReplicas=$(kubectl get statefulset "stackstate-otel-collector" --output=jsonpath="{.spec.replicas}")
 kubectl scale statefulsets "stackstate-otel-collector" --replicas=0
-set +e
-kubectl exec "stackstate-clickhouse-shard0-0" -c backup -- bash -c "clickhouse-backup --config /etc/clickhouse-backup.yaml restore_remote ${BACKUP_NAME}"
-restoreStatus=$?
-set -e
-kubectl scale statefulsets "stackstate-otel-collector" --replicas="${collectorReplicas}"
 
-if [ $restoreStatus -eq 0 ]
-then
-  echo "Data restored"
-  exit 0
-else
-  echo "[ERROR] Restore failed, please check logs above"
-  exit 1
-fi
+echo "=== Allowing pods to terminate"
+sleep 15
+
+echo "=== Starting the restore operation"
+kubectl exec "stackstate-clickhouse-shard0-0" -c backup -- bash -c "curl -s -X POST http://localhost:7171/backup/download/${BACKUP_NAME}?callback=http://localhost:7171/backup/restore/${BACKUP_NAME}"
+
+echo "=== Restore operation is async, waiting until completion ..."
+until kubectl exec "stackstate-clickhouse-shard0-0" -c backup -- bash -c "curl -s localhost:7171/backup/actions?filter=restore | jq 'select(.command==\"restore ${BACKUP_NAME}\")' | jq -s . | jq -e -r '.[-1].status == \"success\"'"
+do
+  echo "waiting..."
+  sleep 5
+done
+
+echo "=== Scaling up all OTel collectors"
+kubectl scale statefulsets "stackstate-otel-collector" --replicas="${collectorReplicas}"
+echo "=== Make sure that there is at least on replica of opentelemetry-collector"
