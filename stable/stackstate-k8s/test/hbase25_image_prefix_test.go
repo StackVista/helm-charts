8bcc4c825c4d29eb2452a005366875ca62a46b51
Remco Beckers <remco.beckers@suse.com>
2024-09-05 08:16:26 +0200

    STAC-21683 Fix tests

--- stable/stackstate-k8s/test/hbase25_image_prefix_test.go
+++ stable/stackstate-k8s/test/hbase25_image_prefix_test.go
@@ -14,65 +14,7 @@ func TestRegularImageNonSplit(t *testing.T) {
 	resources := helmtestutil.NewKubernetesResources(t, output)
 
 	var expectedDeployments = make(map[string]string)
-	expectedDeployments["stackstate-k8s-server"] = "stackstate-server:6.0.0"
-
-	var foundDeployments = make(map[string]appsv1.Deployment)
-
-	for _, deployment := range resources.Deployments {
-		if _, ok := expectedDeployments[deployment.Name]; ok {
-			foundDeployments[deployment.Name] = deployment
-		}
-	}
-
-	assert.Equal(t, len(expectedDeployments), len(foundDeployments))
-
-	for deploymentName, expectedImagePrefix := range expectedDeployments {
-		assert.Contains(t, foundDeployments[deploymentName].Spec.Template.Spec.Containers[0].Image, expectedImagePrefix)
-	}
-}
-
-func TestRegularImageSplit(t *testing.T) {
-	output := helmtestutil.RenderHelmTemplate(t, "stackstate-k8s", "values/full.yaml")
-
-	resources := helmtestutil.NewKubernetesResources(t, output)
-
-	var expectedDeployments = make(map[string]string)
-	expectedDeployments["stackstate-k8s-api"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-checks"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-health-sync"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-initializer"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-notification"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-state"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-sync"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-
-	var foundDeployments = make(map[string]appsv1.Deployment)
-
-	for _, deployment := range resources.Deployments {
-		if _, ok := expectedDeployments[deployment.Name]; ok {
-			foundDeployments[deployment.Name] = deployment
-		}
-	}
-
-	assert.Equal(t, len(expectedDeployments), len(foundDeployments))
-
-	for deploymentName, expectedImageRegex := range expectedDeployments {
-		assert.NotRegexp(t, expectedImageRegex, foundDeployments[deploymentName].Spec.Template.Spec.Containers[0].Image)
-	}
-}
-
-func TestHbase25ImageSplit(t *testing.T) {
-	output := helmtestutil.RenderHelmTemplate(t, "stackstate-k8s", "values/full.yaml", "values/hbase25_enabled.yaml")
-
-	resources := helmtestutil.NewKubernetesResources(t, output)
-
-	var expectedDeployments = make(map[string]string)
-	expectedDeployments["stackstate-k8s-api"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-checks"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-health-sync"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-initializer"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-notification"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-state"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
-	expectedDeployments["stackstate-k8s-sync"] = ".*stackstate-server:6\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-server"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
 
 	var foundDeployments = make(map[string]appsv1.Deployment)
 
@@ -86,5 +28,63 @@ func TestHbase25ImageSplit(t *testing.T) {
 
 	for deploymentName, expectedImageRegex := range expectedDeployments {
 		assert.Regexp(t, expectedImageRegex, foundDeployments[deploymentName].Spec.Template.Spec.Containers[0].Image)
+	}
+}
+
+func TestRegularImageSplit(t *testing.T) {
+	output := helmtestutil.RenderHelmTemplate(t, "stackstate-k8s", "values/full.yaml")
+
+	resources := helmtestutil.NewKubernetesResources(t, output)
+
+	var expectedDeployments = make(map[string]string)
+	expectedDeployments["stackstate-k8s-api"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-checks"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-health-sync"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-initializer"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-notification"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-state"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-sync"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+
+	var foundDeployments = make(map[string]appsv1.Deployment)
+
+	for _, deployment := range resources.Deployments {
+		if _, ok := expectedDeployments[deployment.Name]; ok {
+			foundDeployments[deployment.Name] = deployment
+		}
+	}
+
+	assert.Equal(t, len(expectedDeployments), len(foundDeployments))
+
+	for deploymentName, expectedImageRegex := range expectedDeployments {
+		assert.Regexp(t, expectedImageRegex, foundDeployments[deploymentName].Spec.Template.Spec.Containers[0].Image)
+	}
+}
+
+func TestHbase12ImageSplit(t *testing.T) {
+	output := helmtestutil.RenderHelmTemplate(t, "stackstate-k8s", "values/full.yaml", "values/hbase12_enabled.yaml")
+
+	resources := helmtestutil.NewKubernetesResources(t, output)
+
+	var expectedDeployments = make(map[string]string)
+	expectedDeployments["stackstate-k8s-api"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-checks"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-health-sync"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-initializer"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-notification"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-state"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+	expectedDeployments["stackstate-k8s-sync"] = ".*stackstate-server:\\d\\.0\\.0-snapshot.*-2\\.5"
+
+	var foundDeployments = make(map[string]appsv1.Deployment)
+
+	for _, deployment := range resources.Deployments {
+		if _, ok := expectedDeployments[deployment.Name]; ok {
+			foundDeployments[deployment.Name] = deployment
+		}
+	}
+
+	assert.Equal(t, len(expectedDeployments), len(foundDeployments))
+
+	for deploymentName, expectedImageRegex := range expectedDeployments {
+		assert.NotRegexp(t, expectedImageRegex, foundDeployments[deploymentName].Spec.Template.Spec.Containers[0].Image)
 	}
 }
