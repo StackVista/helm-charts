2422b4c29c0440f31b7ca5da27bbb9c16250c0fd
Remco Beckers <remco.beckers@suse.com>
2024-09-05 18:15:11 +0200

    STAC-21569 Support chart directory in get images so it can be used in our CI

--- stable/suse-observability-agent/installation/o11y-agent-get-images.sh
+++ stable/suse-observability-agent/installation/o11y-agent-get-images.sh
@@ -12,19 +12,23 @@ function usage() {
 Get the list of Docker images used by the SUSE Observability Agent chart
 
 Usage:
-  $0 -f suse-observability-agent-X.Y.Z.tgz
+  $0 -f stackstate-k8s-agent-X.Y.Z.tgz
 
 Arguments:
-    -f : TGZ archive with the SUSE Observability Agent chart (required)
+    -f : TGZ archive with the SUSE Observability Agent chart (one of -f or -d is required)
+    -d : Directory with SUSE Observability Agent chart (one of -f or -d is required)
     -h : Show this help text
 EOF
 }
 
 # Parse options
-while getopts ":f:h" opt; do
+while getopts ":f:d:h" opt; do
   case ${opt} in
     f)
       helm_chart_archive=${OPTARG}
+      ;;
+    d)
+      helm_chart_dir=${OPTARG}
       ;;
     h)
       usage
@@ -45,24 +49,36 @@ done
 
 
 # Check if all required options are provided
-if [[ -z "${helm_chart_archive}" ]]; then
-  echo -e "${RED}Error: -f option is required.${NO_COLOR}" >&2
+if [[ -z "${helm_chart_archive}" ]] && [[ -z "$helm_chart_dir" ]]; then
+  echo -e "${RED}Error: -f or -d option is required.${NO_COLOR}" >&2
   usage
   exit 1
 fi
 
 # Check if the archive exists
-if [ ! -f "${helm_chart_archive}" ]; then
+if [[ -n "${helm_chart_archive}" ]] && [ ! -f "${helm_chart_archive}" ]; then
   echo -e "${RED}Helm chart archive not found${NO_COLOR}: ${helm_chart_archive}" >&2
   exit 1
 fi
 
-# Helm values to enable non-deafult features and get their images.
-helm_values="http-header-injector-webhook.enabled=true,stackstate.apiKey=APIKEY,logsAgent.enabled=true,stackstate.cluster.name=dummy-cluster,stackstate.url=http://dummy.stackstate.io"
+# Check if the dir exists
+if [[ -n "${helm_chart_dir}" ]] && [ ! -d "${helm_chart_dir}" ]; then
+  echo -e "${RED}Helm chart directory not found${NO_COLOR}: ${helm_chart_dir}" >&2
+  exit 1
+fi
+
+# Helm values to enable non-default features and get their images.
+helm_values="httpHeaderInjectorWebhook.enabled=true,stackstate.apiKey=APIKEY,logsAgent.enabled=true,stackstate.cluster.name=dummy-cluster,stackstate.url=http://dummy.stackstate.io"
 helm_release=release
 
+if [[ -z ${helm_chart_archive} ]]; then
+  helm_chart="${helm_chart_dir}"
+else
+  helm_chart="${helm_chart_archive}"
+fi
+
 # Render the manifests from the Helm chart, skipping known warnings.
-helm_manifests=$(helm template ${helm_release} "${helm_chart_archive}" --set "${helm_values}" 2>/dev/stdout | grep -Ev "coalesce.go:")
+helm_manifests=$(helm template ${helm_release} "${helm_chart}" --set "${helm_values}" 2>/dev/stdout | grep -Ev "coalesce.go:")
 # shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
   echo "${helm_manifests}"
