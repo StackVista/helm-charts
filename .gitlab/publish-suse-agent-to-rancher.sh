79d97ba716630f3ed4e7f5634983fbea7224b7a1
Remco Beckers <remco.beckers@suse.com>
2024-09-06 12:17:48 +0200

    STAC-21659 Rename docker images properly (without stacsktate namespace) and skip the intermediate tar.gz file.

--- .gitlab/publish-suse-agent-to-rancher.sh
+++ .gitlab/publish-suse-agent-to-rancher.sh
@@ -3,14 +3,49 @@
 cd stable/suse-observability-agent || exit
 
 echo "Pushing container images to Rancher container registry"
-./installation/o11y-agent-get-images.sh -d . > "o11y-agent-images.txt"
-./installation/o11y-agent-save-images.sh
-DST_REGISTRY_USERNAME=$RANCHER_CONTAINER_REGISTRY_USERNAME DST_REGISTRY_PASSWORD=$RANCHER_CONTAINER_REGISTRY_PASSWORD o11y-agent-load-images.sh -d "$RANCHER_CONTAINER_REGISTRY_URL"
+helm dependencies update .
+
+image_list_file="o11y-agent-images.txt"
+
+./installation/o11y-agent-get-images.sh -d . > "${image_list_file}"
+
+# Pull the images from the list
+while IFS= read -r image; do
+  if docker pull "${image}" > /dev/null 2>&1; then
+    echo -e "${GREEN}Image pull success${NO_COLOR}: ${image}"
+    pulled="${pulled} ${image}"
+  else
+    echo -e "${RED}Image pull failed${NO_COLOR}: ${image}"
+  fi
+done < "${image_list_file}"
+
+# Login to the rancher registry
+if [ -n "$RANCHER_CONTAINER_REGISTRY_USERNAME" ] && [ -n "$RANCHER_CONTAINER_REGISTRY_PASSWORD" ]; then
+  docker login -u "$RANCHER_CONTAINER_REGISTRY_USERNAME" -p "$RANCHER_CONTAINER_REGISTRY_PASSWORD" "$RANCHER_CONTAINER_REGISTRY_URL"
+fi
+
+# Push images
+while IFS= read -r image; do
+  # Simple check if the image is in the format <registry>/<namespace>/<repository>:<tag>
+  if [[ $image =~ ^([^/]+)/([^/]+)/(.*):([^:]+)$ ]]; then
+    repository_and_tag=$(echo "${image}" | cut -d'/' -f3-)
+    dest_image="$RANCHER_CONTAINER_REGISTRY_URL/$RANCHER_CONTAINER_REGISTRY_NAMESPACE/${repository_and_tag}"
+    docker tag "${image}" "${dest_image}"
+    docker push "${dest_image}"
+    # shellcheck disable=SC2181
+    if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Successfully pushed ${dest_image}${NO_COLOR}"
+    else
+      echo -e "${RED}Failed to push ${dest_image}${NO_COLOR}"
+    fi
+  else
+      echo -e "${RED}Image url ${image} is not valid. Skipping...${NO_COLOR}"
+  fi
+done < "${image_list_file}"
 
 echo "Reconfiguring container images registry in values.yaml"
 yq -i -e  '.global.imageRegistry = env(RANCHER_CONTAINER_REGISTRY_URL)' values.yaml
 sed -i -r "s|(\s+repository:\s+)stackstate/|\1${RANCHER_CONTAINER_REGISTRY_NAMESPACE}/|g" values.yaml
 
 echo "Packaging and publishing helm chart..."
-helm dependencies update .
 helm cm-push --username "$RANCHER_HELM_REGISTRY_USERNAME" --password "$RANCHER_HELM_REGISTRY_PASSWORD" . "$RANCHER_HELM_REGISTRY_URL"
