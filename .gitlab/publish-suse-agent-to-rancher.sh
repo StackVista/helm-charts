3c68fc13b97923ee6651c54079475a7ae0f5965f
Remco Beckers <remco.beckers@suse.com>
2024-09-06 13:38:45 +0200

    STAC-21659 Remove images after pushing to avoid filling up disk on runners

--- .gitlab/publish-suse-agent-to-rancher.sh
+++ .gitlab/publish-suse-agent-to-rancher.sh
@@ -9,25 +9,22 @@ image_list_file="o11y-agent-images.txt"
 
 ./installation/o11y-agent-get-images.sh -d . > "${image_list_file}"
 
-# Pull the images from the list
-while IFS= read -r image; do
-  if docker pull "${image}" > /dev/null 2>&1; then
-    echo -e "${GREEN}Image pull success${NO_COLOR}: ${image}"
-    pulled="${pulled} ${image}"
-  else
-    echo -e "${RED}Image pull failed${NO_COLOR}: ${image}"
-  fi
-done < "${image_list_file}"
-
 # Login to the rancher registry
 if [ -n "$RANCHER_CONTAINER_REGISTRY_USERNAME" ] && [ -n "$RANCHER_CONTAINER_REGISTRY_PASSWORD" ]; then
   docker login -u "$RANCHER_CONTAINER_REGISTRY_USERNAME" -p "$RANCHER_CONTAINER_REGISTRY_PASSWORD" "$RANCHER_CONTAINER_REGISTRY_URL"
 fi
 
-# Push images
+# Pull the images from the list
 while IFS= read -r image; do
   # Simple check if the image is in the format <registry>/<namespace>/<repository>:<tag>
   if [[ $image =~ ^([^/]+)/([^/]+)/(.*):([^:]+)$ ]]; then
+    if docker pull "${image}" > /dev/null 2>&1; then
+      echo -e "${GREEN}Image pull success${NO_COLOR}: ${image}"
+      pulled="${pulled} ${image}"
+    else
+      echo -e "${RED}Image pull failed${NO_COLOR}: ${image}"
+    fi
+
     repository_and_tag=$(echo "${image}" | cut -d'/' -f3-)
     dest_image="$RANCHER_CONTAINER_REGISTRY_URL/$RANCHER_CONTAINER_REGISTRY_NAMESPACE/${repository_and_tag}"
     docker tag "${image}" "${dest_image}"
@@ -38,6 +35,9 @@ while IFS= read -r image; do
     else
       echo -e "${RED}Failed to push ${dest_image}${NO_COLOR}"
     fi
+
+    # Remove the image again to not fill up all diskspace on the runner
+    docker rmi "${image}"
   else
       echo -e "${RED}Image url ${image} is not valid. Skipping...${NO_COLOR}"
   fi
