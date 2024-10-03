c60a03c74d5700c8c7baa364e142b56739a41e65
Remco Beckers <remco.beckers@suse.com>
2024-10-03 08:50:58 +0200

    STAC-21755 Fix log following

--- stable/stackstate-k8s/restore/backup-configuration-now.sh
+++ stable/stackstate-k8s/restore/backup-configuration-now.sh
@@ -6,4 +6,4 @@ JOB_NAME="${CRONJOB_NAME}-manual-$(date +%Y%m%dt%H%M%S)"
 
 kubectl create job --from=cronjob/"${CRONJOB_NAME}" "${JOB_NAME}"
 
-while ! kubectl logs "job.batch/${JOB_NAME}"  --container=backup-settings >/dev/null 2>/dev/null ; do echo "Waiting for job to start..."; sleep 2; done; kubectl logs "job/${JOB_NAME}"  --container=backup-settings --follow=true
+while ! kubectl logs "job/${JOB_NAME}" >/dev/null 2>/dev/null ; do echo "Waiting for job to start..."; sleep 2; done; kubectl logs "job/${JOB_NAME}"   --follow=true
