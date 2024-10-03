8c9c1a2cc9cf267a901f84a13a2eb6c5bcd1f26e
Remco Beckers <remco.beckers@suse.com>
2024-10-03 16:40:30 +0200

    STAC-21755 Fix backup now script

--- stable/suse-observability/restore/backup-configuration-now.sh
+++ stable/suse-observability/restore/backup-configuration-now.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-CRONJOB_NAME="suse-observability-backup-conf"
-JOB_NAME="${CRONJOB_NAME}-manual-$(date +%Y%m%dt%H%M%S)"
+CRONJOB_NAME=$(get cronjob -l app.kubernetes.io/component=backup-settings -l app.kubernetes.io/name=suse-observability)
+JOB_NAME="suse-observability-manual-$(date +%Y%m%dt%H%M%S)"
 
-kubectl create job --from=cronjob/"${CRONJOB_NAME}" "${JOB_NAME}"
+kubectl create job --from="${CRONJOB_NAME}" "${JOB_NAME}"
 
 while ! kubectl logs "job/${JOB_NAME}" >/dev/null 2>/dev/null ; do echo "Waiting for job to start..."; sleep 2; done; kubectl logs "job/${JOB_NAME}"   --follow=true
