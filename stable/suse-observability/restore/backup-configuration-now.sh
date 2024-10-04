49101ca402633b1665dcc38ba9f69fa6f6122688
Remco Beckers <remco.beckers@suse.com>
2024-10-04 16:17:12 +0200

    STAC-21755 Rename job

--- stable/suse-observability/restore/backup-configuration-now.sh
+++ stable/suse-observability/restore/backup-configuration-now.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 CRONJOB_NAME=$(kubectl get cronjob -l app.kubernetes.io/component=backup-settings -l app.kubernetes.io/name=suse-observability -o name)
-JOB_NAME="suse-observability-manual-$(date +%Y%m%dt%H%M%S)"
+JOB_NAME="suse-observability-backup-conf-manual-$(date +%Y%m%dt%H%M%S)"
 
 kubectl create job --from="${CRONJOB_NAME}" "${JOB_NAME}"
 
