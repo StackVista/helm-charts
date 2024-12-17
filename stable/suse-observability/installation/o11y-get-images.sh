186c05914c59f8257d85340403c2d7d22124a591
Bram Schuur <bschuur@stackstate.com>
2024-12-17 11:29:29 +0100

    WIP

--- stable/suse-observability/installation/o11y-get-images.sh
+++ stable/suse-observability/installation/o11y-get-images.sh
@@ -6,25 +6,34 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NO_COLOR='\033[0m'
 
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function usage() {
   cat <<EOF >&2
 Get the list of Docker images used by the Rancher Prime Observability chart
 
 Usage:
-  $0 -f stackstate-k8s-X.Y.Z.tgz
+  $0 [-f suse-observability-X.Y.Z.tgz]
 
 Arguments:
-    -f : TGZ archive with the Rancher Prime Observability chart (required)
+    -f : TGZ archive with the Rancher Prime Observability chart (optional)
     -h : Show this help text
 EOF
 }
+
+helm_chart_archive=$(realpath "$dir/..")
 
 # Parse options
 while getopts ":f:h" opt; do
   case ${opt} in
     f)
       helm_chart_archive=${OPTARG}
+
+      # Check if the archive exists
+      if [ ! -f "${helm_chart_archive}" ]; then
+        echo -e "${RED}Helm chart archive not found${NO_COLOR}: ${helm_chart_archive}" >&2
+        exit 1
+      fi
       ;;
     h)
       usage
@@ -43,25 +52,12 @@ while getopts ":f:h" opt; do
   esac
 done
 
-# Check if all required options are provided
-if [[ -z "${helm_chart_archive}" ]]; then
-  echo -e "${RED}Error: -f option is required.${NO_COLOR}" >&2
-  usage
-  exit 1
-fi
-
-# Check if the archive exists
-if [ ! -f "${helm_chart_archive}" ]; then
-  echo -e "${RED}Helm chart archive not found${NO_COLOR}: ${helm_chart_archive}" >&2
-  exit 1
-fi
-
 helm_release=release
 
 images=()
 function listImages() {
   tmp_file=/tmp/o11y-tenant-get-images
-  helm_values_base="anomaly-detection.enabled=true,backup.enabled=true,victoria-metrics-0.backup.enabled=true,minio.accessKey=ABCDEFGH,minio.secretKey=ABCDEFGHABCDEFGH,stackstate.baseUrl=http://dummy.stackstate.io,stackstate.admin.authentication.password=dummy,stackstate.authentication.adminPassword=dummy,stackstate.license.key=dummy,global.receiverApiKey=dummy,clickhouse.enabled=true,opentelemetry.enabled=true"
+  helm_values_base="anomaly-detection.enabled=true,backup.enabled=true,victoria-metrics-0.backup.enabled=true,victoria-metrics-0.restore.enabled=true,minio.accessKey=ABCDEFGH,minio.secretKey=ABCDEFGHABCDEFGH,stackstate.baseUrl=http://dummy.stackstate.io,stackstate.admin.authentication.password=dummy,stackstate.authentication.adminPassword=dummy,stackstate.license.key=dummy,global.receiverApiKey=dummy,clickhouse.enabled=true,opentelemetry.enabled=true"
   # hbase in Distributed mode
   helm_values="$helm_values_base"
   helm template "$helm_release" "$helm_chart_archive" --set "$helm_values" | grep image: | sed -E 's/^.*image: ['\''"]?([^'\''"]*)['\''"]?.*$/\1/' > "$tmp_file"
---
+++ stable/suse-observability/maintenance/change-image-source.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+set -o pipefail
+
+# shellcheck disable=SC2034
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function usage() {
+  cat <<EOF >&2
+Change the registry and repository in the helm chart values file. This is meant as a pre-publishing step when the chart
+gets published to an alternative repository.
+
+Usage:
+  $0 -g docker_registry -p docker_repository
+
+Arguments:
+    -g : Docker registry to change to
+    -p : Docker repository to change to
+    -h : Show this help text
+EOF
+}
+
+# Parse options
+while getopts ":g:p:h" opt; do
+  case ${opt} in
+    g)
+        docker_registry_to=${OPTARG}
+        ;;
+    p)
+        docker_repo_to=${OPTARG}
+        ;;
+    h)
+      usage
+      exit 0
+      ;;
+    :)
+      echo -e "${RED}Option -${OPTARG} requires an argument.${NO_COLOR}" >&2
+      usage
+      exit 1
+      ;;
+    *)
+      echo -e "${RED}Unimplemented option: -${OPTARG}${NO_COLOR}" >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+helm_chart_dir=$(realpath "$dir/..")
+
+# Check if all required options are provided
+if [[ -z "${docker_registry_to}" ]]; then
+  echo -e "${RED}Error: -g option is required.${NO_COLOR}" >&2
+  usage
+  exit 1
+fi
+
+if [[ -z "${docker_repo_to}" ]]; then
+  echo -e "${RED}Error: -p option is required.${NO_COLOR}" >&2
+  usage
+  exit 1
+fi
+
+function updateDockerImagesInValues() {
+  echo "Updating values.yaml to use new docker images"
+  sed -i "s#registry: [^/:]\+\$#registry: $docker_registry_to#g" "$helm_chart_dir/values.yaml"
+  sed -i "s#repository: [^/:]\+\/[^/:]\+\/\([^/:]\+\)\$#repository: $docker_registry_to/$docker_repo_to/\1#g" "$helm_chart_dir/values.yaml"
+  sed -i "s#repository: [^/:]\+\/\([^/:]\+\)\$#repository: $docker_repo_to/\1#g" "$helm_chart_dir/values.yaml"
+  sed -i "s#spotlightRepository: [^/:]\+\/\([^/:]\+\)\$#spotlightRepository: $docker_repo_to/\1#g" "$helm_chart_dir/values.yaml"
+
+  if [ "$helm_chart_dir" == "suse-observability" ]; then
+    # Update elasticsearch
+    yq e -i ".elasticsearch.imageRegistry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".elasticsearch.imageRepository = \"$docker_repo_to/elasticsearch\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".elasticsearch.prometheus-elasticsearch-exporter.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".elasticsearch.prometheus-elasticsearch-exporter.image.repository = \"$docker_repo_to/elasticsearch-exporter\"" "$helm_chart_dir/values.yaml"
+    # Update hbase
+    yq e -i ".hbase.all.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.wait.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.zookeeper.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.stackgraph.image.repository = \"$docker_repo_to/stackgraph-hbase\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.console.image.repository = \"$docker_repo_to/stackgraph-console\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.wait.image.repository = \"$docker_repo_to/wait\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.hbase.master.image.repository = \"$docker_repo_to/hbase-master\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.hbase.regionserver.image.repository = \"$docker_repo_to/hbase-regionserver\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.hdfs.image.repository = \"$docker_repo_to/hadoop\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.tephra.image.repository = \"$docker_repo_to/tephra-server\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".hbase.zookeeper.image.repository = \"$docker_repo_to/zookeeper\"" "$helm_chart_dir/values.yaml"
+    # Update Victoria Metrics
+    yq e -i ".victoria-metrics-0.server.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-0.server.image.repository = \"$docker_repo_to/victoria-metrics\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-1.server.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-1.server.image.repository = \"$docker_repo_to/victoria-metrics\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-0.backup.vmbackup.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-0.backup.vmbackup.image.repository = \"$docker_repo_to/vmbackup\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-1.backup.vmbackup.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-1.backup.vmbackup.image.repository = \"$docker_repo_to/vmbackup\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-0.backup.setupCron.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-0.backup.setupCron.image.repository = \"$docker_repo_to/container-tools\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-1.backup.setupCron.image.registry = \"$docker_registry_to\"" "$helm_chart_dir/values.yaml"
+    yq e -i ".victoria-metrics-1.backup.setupCron.image.repository = \"$docker_repo_to/container-tools\"" "$helm_chart_dir/values.yaml"
+  fi
+
+  echo "Updating documentations (README.md)"
+  (cd  "$helm_chart_dir" && helm-docs)
+}
+
+updateDockerImagesInValues
