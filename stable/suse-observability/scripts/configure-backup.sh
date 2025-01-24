3d7146e0cec7e6ea6996d846446209b9be227940
Alejandro Acevedo <alejandro.acevedo@suse.com>
2025-01-24 09:31:33 +0100

    STAC-21887: Add a workdir to only the jobs that really need it

--- stable/suse-observability/scripts/configure-backup.sh
+++ stable/suse-observability/scripts/configure-backup.sh
@@ -6,13 +6,13 @@ AWS_ACCESS_KEY_ID="$(cat /aws-keys/accesskey)"
 export AWS_SECRET_ACCESS_KEY
 AWS_SECRET_ACCESS_KEY="$(cat /aws-keys/secretkey)"
 export MC_HOST_minio="http://${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}@${MINIO_ENDPOINT}"
-
+MC_WITH_CONFIG_DIR="/work/.mc"
 if [ "${BACKUP_STACKGRAPH_RESTORE_ENABLED}" == "true" ] || [ "${BACKUP_STACKGRAPH_SCHEDULED_ENABLED}" == "true" ]; then
     echo "=== Testing for existence of MinIO bucket \"${BACKUP_STACKGRAPH_BUCKET_NAME}\"..."
-    if ! mc ls "minio/${BACKUP_STACKGRAPH_BUCKET_NAME}/${BACKUP_STACKGRAPH_S3_PREFIX}" >/dev/null ; then
+    if ! mc -C "$MC_WITH_CONFIG_DIR" ls "minio/${BACKUP_STACKGRAPH_BUCKET_NAME}/${BACKUP_STACKGRAPH_S3_PREFIX}" >/dev/null ; then
         if [ "${BACKUP_STACKGRAPH_SCHEDULED_ENABLED}" == "true" ]; then
             echo "=== Creating MinIO bucket \"${BACKUP_STACKGRAPH_BUCKET_NAME}\"..."
-            mc mb "minio/${BACKUP_STACKGRAPH_BUCKET_NAME}"
+            mc -C "$MC_WITH_CONFIG_DIR" mb "minio/${BACKUP_STACKGRAPH_BUCKET_NAME}"
         else
             echo "=== ERROR: MinIO bucket \"${BACKUP_STACKGRAPH_BUCKET_NAME}\" does not exist. Restore functionality for StackGraph will not function correctly"
             exit 1
@@ -22,10 +22,10 @@ fi
 
 if [ "${BACKUP_CONFIGURATION_RESTORE_ENABLED}" == "true" ] || [ "${BACKUP_CONFIGURATION_SCHEDULED_ENABLED}" == "true" ]; then
     echo "=== Testing for existence of MinIO bucket \"${BACKUP_CONFIGURATION_BUCKET_NAME}\"..."
-    if ! mc ls "minio/${BACKUP_CONFIGURATION_BUCKET_NAME}/${BACKUP_CONFIGURATION_S3_PREFIX}" >/dev/null ; then
+    if ! mc -C "$MC_WITH_CONFIG_DIR" ls "minio/${BACKUP_CONFIGURATION_BUCKET_NAME}/${BACKUP_CONFIGURATION_S3_PREFIX}" >/dev/null ; then
         if [ "${BACKUP_CONFIGURATION_SCHEDULED_ENABLED}" == "true" ]; then
             echo "=== Creating MinIO bucket \"${BACKUP_CONFIGURATION_BUCKET_NAME}\"..."
-            mc mb "minio/${BACKUP_CONFIGURATION_BUCKET_NAME}"
+            mc -C "$MC_WITH_CONFIG_DIR" mb "minio/${BACKUP_CONFIGURATION_BUCKET_NAME}"
         else
             echo "=== ERROR: MinIO bucket \"${BACKUP_CONFIGURATION_BUCKET_NAME}\" does not exist. Restore functionality for configuration will not function correctly"
             exit 1
@@ -35,10 +35,10 @@ fi
 
 if [ "${BACKUP_VICTORIA_METRICS_0_RESTORE_ENABLED}" == "true" ] || [ "${BACKUP_VICTORIA_METRICS_0_ENABLED}" == "true" ]; then
     echo "=== Testing for existence of MinIO bucket \"${BACKUP_VICTORIA_METRICS_0_BUCKET_NAME}\"..."
-    if ! mc ls "minio/${BACKUP_VICTORIA_METRICS_0_BUCKET_NAME}/${BACKUP_VICTORIA_METRICS_1_S3_PREFIX}" >/dev/null ; then
+    if ! mc -C "$MC_WITH_CONFIG_DIR" ls "minio/${BACKUP_VICTORIA_METRICS_0_BUCKET_NAME}/${BACKUP_VICTORIA_METRICS_1_S3_PREFIX}" >/dev/null ; then
         if [ "${BACKUP_VICTORIA_METRICS_0_ENABLED}" == "true" ]; then
             echo "=== Creating MinIO bucket \"${BACKUP_VICTORIA_METRICS_0_BUCKET_NAME}\"..."
-            mc mb "minio/${BACKUP_VICTORIA_METRICS_0_BUCKET_NAME}"
+            mc -C "$MC_WITH_CONFIG_DIR" mb "minio/${BACKUP_VICTORIA_METRICS_0_BUCKET_NAME}"
         else
             echo "=== ERROR: MinIO bucket \"${BACKUP_VICTORIA_METRICS_0_BUCKET_NAME}\" does not exist. Restore functionality for VictoriaMetrics_0 will not function correctly"
             exit 1
@@ -48,10 +48,10 @@ fi
 
 if [ "${BACKUP_VICTORIA_METRICS_1_RESTORE_ENABLED}" == "true" ] || [ "${BACKUP_VICTORIA_METRICS_1_ENABLED}" == "true" ]; then
     echo "=== Testing for existence of MinIO bucket \"${BACKUP_VICTORIA_METRICS_1_BUCKET_NAME}\"..."
-    if ! mc ls "minio/${BACKUP_VICTORIA_METRICS_1_BUCKET_NAME}/${BACKUP_VICTORIA_METRICS_1_S3_PREFIX}" >/dev/null ; then
+    if ! mc -C "$MC_WITH_CONFIG_DIR" ls "minio/${BACKUP_VICTORIA_METRICS_1_BUCKET_NAME}/${BACKUP_VICTORIA_METRICS_1_S3_PREFIX}" >/dev/null ; then
         if [ "${BACKUP_VICTORIA_METRICS_1_ENABLED}" == "true" ]; then
             echo "=== Creating MinIO bucket \"${BACKUP_VICTORIA_METRICS_1_BUCKET_NAME}\"..."
-            mc mb "minio/${BACKUP_VICTORIA_METRICS_1_BUCKET_NAME}"
+            mc -C "$MC_WITH_CONFIG_DIR" mb "minio/${BACKUP_VICTORIA_METRICS_1_BUCKET_NAME}"
         else
             echo "=== ERROR: MinIO bucket \"${BACKUP_VICTORIA_METRICS_1_BUCKET_NAME}\" does not exist. Restore functionality for VictoriaMetrics_1 will not function correctly"
             exit 1
@@ -61,10 +61,10 @@ fi
 
 if [ "${BACKUP_CLICKHOUSE_RESTORE_ENABLED}" == "true" ] || [ "${BACKUP_CLICKHOUSE_SCHEDULED_ENABLED}" == "true" ]; then
     echo "=== Testing for existence of MinIO bucket \"${BACKUP_CLICKHOUSE_BUCKET_NAME}\"..."
-    if ! mc ls "minio/${BACKUP_CLICKHOUSE_BUCKET_NAME}/${BACKUP_CLICKHOUSE_S3_PREFIX}" >/dev/null ; then
+    if ! mc -C "$MC_WITH_CONFIG_DIR" ls "minio/${BACKUP_CLICKHOUSE_BUCKET_NAME}/${BACKUP_CLICKHOUSE_S3_PREFIX}" >/dev/null ; then
         if [ "${BACKUP_CLICKHOUSE_SCHEDULED_ENABLED}" == "true" ]; then
             echo "=== Creating MinIO bucket \"${BACKUP_CLICKHOUSE_BUCKET_NAME}\"..."
-            mc mb "minio/${BACKUP_CLICKHOUSE_BUCKET_NAME}"
+            mc -C "$MC_WITH_CONFIG_DIR" mb "minio/${BACKUP_CLICKHOUSE_BUCKET_NAME}"
         else
             echo "=== ERROR: MinIO bucket \"${BACKUP_CLICKHOUSE_BUCKET_NAME}\" does not exist. Restore functionality for ClickHouse will not function correctly"
             exit 1
@@ -74,10 +74,10 @@ fi
 
 if [ "${BACKUP_ELASTICSEARCH_RESTORE_ENABLED}" == "true" ] || [ "${BACKUP_ELASTICSEARCH_SCHEDULED_ENABLED}" == "true" ]; then
     echo "=== Testing for existence of MinIO bucket \"${BACKUP_ELASTICSEARCH_BUCKET_NAME}\"..."
-    if ! mc ls "minio/${BACKUP_ELASTICSEARCH_BUCKET_NAME}/${BACKUP_ELASTICSEARCH_S3_PREFIX}" >/dev/null ; then
+    if ! mc -C "$MC_WITH_CONFIG_DIR" ls "minio/${BACKUP_ELASTICSEARCH_BUCKET_NAME}/${BACKUP_ELASTICSEARCH_S3_PREFIX}" >/dev/null ; then
         if [ "${BACKUP_ELASTICSEARCH_SCHEDULED_ENABLED}" == "true" ]; then
             echo "=== Creating MinIO bucket \"${BACKUP_ELASTICSEARCH_BUCKET_NAME}\"..."
-            mc mb "minio/${BACKUP_ELASTICSEARCH_BUCKET_NAME}"
+            mc -C "$MC_WITH_CONFIG_DIR" mb "minio/${BACKUP_ELASTICSEARCH_BUCKET_NAME}"
         else
             echo "=== ERROR: MinIO bucket \"${BACKUP_ELASTICSEARCH_BUCKET_NAME}\" does not exist. Restore functionality for ElasticSearch will not function correctly"
             exit 1
