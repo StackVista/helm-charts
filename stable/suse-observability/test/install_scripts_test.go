57a04e617840c93558088e8faa636622ee4ce162
Louis Parkin <louis.parkin@suse.com>
2026-02-20 16:03:55 +0100

    STAC-24270 Bump expected test images from 33 to 34.

--- stable/suse-observability/test/install_scripts_test.go
+++ stable/suse-observability/test/install_scripts_test.go
@@ -17,7 +17,7 @@ func TestGetImages(t *testing.T) {
 	curDir, err := os.Getwd()
 	require.NoError(t, err)
 	images := strings.Split(RunGetImagesScript(t, filepath.Join(curDir, "..")), "\n")
-	require.Equal(t, 33, len(
+	require.Equal(t, 34, len(
 		slices.DeleteFunc(
 			images,
 			func(e string) bool {
