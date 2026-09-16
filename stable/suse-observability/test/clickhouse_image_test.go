07b846caa44c3d599643c1f9333f5dd9341c596f
SUSE Observability AI (POC) <suse-observability-ops@suse.com>
2026-09-16 15:31:17 +0000

    Use actual chart resource helper types in image regression test

--- stable/suse-observability/test/clickhouse_image_test.go
+++ stable/suse-observability/test/clickhouse_image_test.go
@@ -12,7 +12,7 @@ func TestClickhouseCleanupUsesServerImage(t *testing.T) {
 	output := helmtestutil.RenderHelmTemplate(t, "suse-observability", "values/full.yaml")
 	resources := helmtestutil.NewKubernetesResources(t, output)
 	var serverImage string
-	for _, sts := range resources.StatefulSets {
+	for _, sts := range resources.Statefulsets {
 		for _, container := range sts.Spec.Template.Spec.Containers {
 			if container.Name == "clickhouse" {
 				serverImage = container.Image
@@ -20,7 +20,7 @@ func TestClickhouseCleanupUsesServerImage(t *testing.T) {
 		}
 	}
 	require.NotEmpty(t, serverImage, "ClickHouse server must be rendered")
-	cleanup := findJob(resources, "ch-clean")
+	cleanup := findJob(&resources, "ch-clean")
 	require.NotNil(t, cleanup, "ClickHouse cleanup hook must be rendered")
 	require.NotEmpty(t, cleanup.Spec.Template.Spec.Containers)
 	require.Equal(t, serverImage, cleanup.Spec.Template.Spec.Containers[0].Image)
